f58f2dfe65bb3bfcdc15cd9bb6aeb732110f8783
Amna Shehzad <shehzadamna21@gmail.com>
2023-12-14 16:47:28 +0500

    AmnaCommit

--- back-end/app.js
+++ back-end/app.js
@@ -20,8 +20,6 @@ const User_Nutritionist_Inbox = require('./models/User-Nutritionist_Inbox Schema
 const Vendor = require('./models/Vendor Schema');
 const VendorBlockReport = require('./models/VendorBlockReport Schema');
 const VendorCollaboration = require('./models/VendorCollaboration Schema');
-
-
 //app.js
 const express = require('express');
 const mongoose = require('mongoose');
