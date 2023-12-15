6c25014c1c012820242bed48f3e3d075f7e71cc6
anumsajid13 <anumsajid13@gmail.com>
2023-12-15 18:42:31 +0500

    adding user_nutritionis_plan

--- back-end/RecipeSeeker/routes/signin_route.js
+++ back-end/RecipeSeeker/routes/signin_route.js
@@ -5,10 +5,10 @@ const jwt = require('jsonwebtoken');
 const RecipeSeeker = require('../../models/RecipeSeekerSchema');
 const router = express.Router();
 
-router.post('/signin', async (req, res) => {
+router.post('/recipeSeeker_signin', async (req, res) => {
     try {
      
-      const recipeSeeker = await RecipeSeeker.findOne({ email: req.body.username });
+      const recipeSeeker = await RecipeSeeker.findOne({ username: req.body.username });
   
       if (!recipeSeeker) {
         return res.status(404).json({ message: 'RecipeSeeker not found' });
@@ -24,9 +24,12 @@ router.post('/signin', async (req, res) => {
       // Generate a JWT token
       const token = jwt.sign({ id: recipeSeeker._id }, process.env.SECRET_KEY, { expiresIn: '1h' });
   
+      console.log("Token: "+token)
       res.json({ message: 'Signin successful', token });
     } catch (error) {
       res.status(500).json({ message: 'Internal server error', error: error.message });
     }
   });
   module.exports = router;
+
+  
