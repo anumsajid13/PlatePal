6203559654aa78c353de2ba2f41e89d23a97d576
Amna Shehzad <shehzadamna21@gmail.com>
2023-12-15 06:16:16 +0500

    commit4?

---
+++ back-end/Vendor/routes/notificationroute.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const router = express.Router();
+const VendorNotification = require('../models/Vendor_Notification');
+const authenticateToken = require('../TokenAuthentication/token_authenticate');
+
+// Endpoint to get paginated, filtered, and sorted notifications for a vendor
+router.get('/notifications', authenticateToken, async (req, res) => {
+    try {
+      const vendorId = req.user.vendorId; 
+      const { page = 1, limit = 30, sortBy,sortOrder, chefId, type } = req.query;
+  
+      
+    const sortOptions = {};
+    if(!sortBy)
+    {
+    sortOptions[sortBy]=1;
+    }
+    if (sortBy && sortBy=='time') {
+      if (!sortOrder) {
+        sortvalue = 1;//asc default
+      } else if (sortOrder === 'oldest') {
+        sortvalue = -1;//desc
+      } else {
+        sortvalue = 1;//sortorder=='newest' 
+      }
+      sortOptions[sortBy] = sortvalue; 
+    } 
+  
+      const filterQuery = { user: vendorId };
+      if (chefId) {
+        filterQuery.chefId = chefId;
+      }
+      if (type) {
+        filterQuery.type = type;
+      }
+  
+      const notifications = await VendorNotification.find(filterQuery)
+        .sort(sortOptions)
+        .skip((page - 1) * limit)
+        .limit(Number(limit))
+        .exec();
+  
+      if (notifications.length === 0) {
+        return res.status(404).json({ message: 'No notifications found for the vendor' });
+      }
+      const notificationNo = await VendorNotification.find(query).countDocuments(); //total no. of notifications in db
+      const totalPages = Math.ceil(notificationNo / limit); //total no. of pages
+      res.status(200).json({ notifications, totalPages });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: 'Internal Server Error' });
+    }
+  });
+  
+  module.exports = router;
