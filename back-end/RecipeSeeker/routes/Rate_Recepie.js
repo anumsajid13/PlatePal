a338d5c79f1ecbee6831289f8692e5d1ff68dcef
anumsajid13 <anumsajid13@gmail.com>
2023-12-17 14:58:51 +0500

    changed navbar color

---
+++ back-end/RecipeSeeker/routes/Rate_Recepie.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const router = express.Router();
+const Rating = require('../../models/Rating Schema');
+const Recipe = require('../../models/Recipe Schema');
+const authenticateToken = require('../../TokenAuthentication/token_authentication');
+
+router.post('/rateRecipe/:recipeId', authenticateToken, async (req, res) => {
+    const { ratingNumber } = req.body;
+    const { recipeId } = req.params;
+    const userId = req.user.id;
+  
+    try {
+      const recipe = await Recipe.findById(recipeId);
+      if (!recipe) {
+        return res.status(404).json({ message: 'Recipe not found' });
+      }
+  
+      // Check if the user has already rated the recipe
+      const existingRating = await Rating.findOne({ user: userId, recipe: recipeId });
+  
+      if (existingRating) {
+        
+        existingRating.ratingNumber = ratingNumber;
+        await existingRating.save();
+      } else {
+       
+        const newRating = new Rating({
+          ratingNumber,
+          user: userId,
+          recipe: recipeId,
+        });
+  
+       
+        await newRating.save();
+        recipe.ratings.push(newRating);
+        await recipe.save();
+      }
+  
+      res.status(201).json({ message: 'Recipe rated successfully' });
+    } catch (error) {
+      console.error('Error rating recipe:', error.message);
+      res.status(500).json({ message: 'Internal server error', error: error.message });
+    }
+  });
+  
+  module.exports = router;
--- front-end/src/RecipeSeeker/components/RecipieCard.jsx
+++ front-end/src/RecipeSeeker/components/RecipieCard.jsx
@@ -1,15 +1,106 @@
 // RecipeCard.js
-import React from 'react';
+import RecipeRating from './RecipeRating.jsx'
+import  { React,useState } from 'react';
+import './RecipeCard.css'
 
 const RecipeCard = ({ recipe }) => {
-  return (
-    <div className="recipe-card">
-      <h3>{recipe.title}</h3>
-      <p>{recipe.description}</p>
-      <p>Chef: {recipe.chef.name}</p>
-      <p>Ratings: {recipe.ratings.length}</p>
-    </div>
-  );
-};
+    const [isPopupOpen, setIsPopupOpen] = useState(false);
+  
+    const handleCardClick = () => {
+        console.log("popup open")
+      setIsPopupOpen(true);
+    };
+  
+    const handleClosePopup = () => {
+      setIsPopupOpen(false);
+    };
+  
+    return (
+      <>
+        <div className="recipe-card" >
+          <>
+          <img src='/pasta.jpg' alt="image here" style={{width: "300px" ,height:"180px"}}></img>
+          </>
+          <div className='flexxx'>
+          <h3 onClick={handleCardClick}>{recipe.title} by Chef {recipe.chef.name}</h3>   
+          <p>{recipe.description}</p>     
+          </div>
+        </div>
+  
+        {/* Popup for detailed recipe view */}
+        <div className={`recipe-popup ${isPopupOpen ? 'popup-open' : ''}`}>
+        <span className="close-btn" onClick={handleClosePopup}>
+            &times;
+        </span>
+        <div className="recipe-details">
+            <div className="recipe-details-left">
+            <img src="/pasta.jpg" alt="image here" />
+            <p>Ingredients:</p>
+            <ul>
+                {recipe.ingredients.map((ingredient, index) => (
+                <li key={index}>
+                    {ingredient.name}: {ingredient.quantity}
+                </li>
+                ))}
+            </ul>
+            <p>Allergens:</p>
+            <ul>
+                {recipe.allergens.map((all, index) => (
+                <li key={index}>
+                    {all}
+                </li>
+                ))}
+            </ul>
+            <p>Required Utensils:</p>
+            <ul>
+                {recipe.utensils.map((uten, index) => (
+                <li key={index}>
+                    {uten}
+                </li>
+                ))}
+            </ul>
 
-export default RecipeCard;
+            <p>Instructions:</p>
+            <ul>
+                {recipe.instructions.map((instruct, index) => (
+                <li key={index}>
+                    {instruct}
+                </li>
+                ))}
+            </ul>
+            
+            </div>
+            <div className="recipe-details-right">
+            <h3 >{recipe.title}</h3>
+            <p>{recipe.description}</p>
+            <p className='recepie-category'>Chef: {recipe.chef.name}</p>
+            <p className='recepie-category'>Serving size: {recipe.servingSize}</p>
+            <p className='recepie-category'>Calories: {recipe.calories}</p>
+            <p className='recepie-category'>Total Time: {recipe.totalTime}</p>
+            <ul className='recepie-category'>
+                {recipe.category.map((cat, index) => (
+                <li key={index}>{cat}</li>
+                ))}
+            </ul>
+            <div className="Rating-class">
+            <p>Ratings: {recipe.ratings.length}</p>
+            <p>Ratings by: {recipe.ratings.user}</p>
+            <RecipeRating
+                recipeId={recipe._id}
+                initialRating={0}
+                onSaveRating={(recipeId, selectedRating) => {
+                // Implement logic to save the rating to the backend
+                console.log(`Recipe ${recipeId} rated with ${selectedRating} stars`);
+                }}
+            />
+            </div>
+            </div>
+        </div>
+        </div>
+        
+      </>
+    );
+  };
+  
+  export default RecipeCard;
+
